6417690a59c8133236754c993a5f6b7166049f67
givatishai <123qwe12>
2016-07-30 14:01:50 +0300

    test commit 3

--- src/main/java/com/example/SampleApplication.java
+++ src/main/java/com/example/SampleApplication.java
@@ -12,7 +12,7 @@ import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 @SpringBootApplication
 public class SampleApplication {
 
-
+//commit
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
